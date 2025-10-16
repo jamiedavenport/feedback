252c964845469b27fd9e53aa969e364a5f27ef94
Jamie Davenport <1329874+jamiedavenport@users.noreply.github.com>
2025-10-16 14:06:52 +0100

    Links

--- src/routes/_authenticated/index.tsx
+++ src/routes/_authenticated/index.tsx
@@ -2,6 +2,7 @@ import { createFileRoute } from "@tanstack/react-router";
 import logo from "@/assets/logo.svg?url";
 import { Keys } from "@/components/keys";
 import { FeedbackTable } from "@/components/table";
+import { Button } from "@/components/ui/button";
 
 export const Route = createFileRoute("/_authenticated/")({
 	component: RouteComponent,
@@ -14,7 +15,27 @@ function RouteComponent() {
 				<img src={logo} alt="Feedback Logo" className="h-10 rounded-xl" />
 				<Keys />
 			</div>
-			<FeedbackTable />
+			<div className="flex items-center gap-3">
+				<Button asChild variant="outline">
+					<a
+						href="https://github.com/jamiedavenport/feedback"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Github
+					</a>
+				</Button>
+				<Button asChild variant="outline">
+					<a
+						href="https://www.npmjs.com/package/@jxdltd/feedback"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						NPM
+					</a>
+				</Button>
+				<FeedbackTable />
+			</div>
 		</div>
 	);
 }
